e36b609e6610635d44393e50e7a88850f7eba298
LeeSeongKyu <80861684+LeeSeongKyu-920220@users.noreply.github.com>
2021-10-08 18:32:54 +0900

    2021/10/08 18:32 (업데이트 테스트)
    테스트

---
+++ Assets/UnitPositioning.cs
@@ -0,0 +1,32 @@
+﻿/*
+=========================================================================================================================
+Title : <유닛 배치 시스템>
+Ver : 1.0
+Date : 2021/10/08 ~
+Content :
+ - 레이 캐스트 활용한 유닛 배치
+ - 유닛끼리는 콜리전을 통해 배치 가능한 공간과 그렇지 않은 공간 둠
+ - 그리드로 구현할지 미지수!
+
+=========================================================================================================================
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnitPositioning : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
