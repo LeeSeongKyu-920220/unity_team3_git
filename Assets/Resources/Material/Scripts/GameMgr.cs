4c214123cbad5678f620cc731f813883a6d42c5c
KGongDol <92151893+KGongDol@users.noreply.github.com>
2021-10-11 15:32:09 +0900

    길찾기
    * 길찾기 AI 입니다.
    
    -목표지점에 빈오브젝트를 만들어 TankCtrl.cs 에 BeginTarPos에 넣어주면 게임플레이시 자동으로 목적지까지 이동합니다.
    -임시로 매터리얼 몇 개 추가했습니다.
    -탱크가 네비메쉬 위에 배치되어 있어야 합니다.
    -목표지점이 네비메쉬 위가 아닐 경우 이동 가능한 가까운 지점을 찾아 이동합니다.

---
+++ Assets/Resources/Material/Scripts/GameMgr.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMgr : MonoBehaviour
+{
+    private LayerMask layerMask = -1;
+    Ray a_MousePos;
+    RaycastHit hit;
+    TankCtrl tankCtrl = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        layerMask = 1 << LayerMask.NameToLayer("Ground");
+
+        // ****************************************************************************************************
+        GameObject.Find("TankRoot"); // 일단 임시로 이름으로 오브젝트 찾아 놓았습니다. 추후 수정하면 됩니다.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
