d02cc9a831999fd9913cfc4e49a8f112073c22e0
LeeSeongKyu <80861684+LeeSeongKyu-920220@users.noreply.github.com>
2021-10-12 19:51:50 +0900

    오브젝트 풀링 구현중

--- Assets/Scripts/UnitPlacing/UnitObjPool.cs
+++ Assets/Scripts/UnitPlacing/UnitObjPool.cs
@@ -10,25 +10,44 @@ public class UnitObjPool : MonoBehaviour
     // 생성할 유닛 오브젝트
     public GameObject[] unitObj;
 
+    // 탱크 오브젝트 풀
+    private Queue<TankCtrl>[] tankPool = new Queue<TankCtrl>[5];
+
     private void Awake()
     {
         unitObjPool = this;
     }
 
-    // Start is called before the first frame update
-    void Start()
+    public TankCtrl CreateNewObj(int objKind)
     {
-        
+        var newObj = Instantiate(unitObj[objKind], transform).GetComponent<TankCtrl>();
+        newObj.gameObject.SetActive(false);
+        return newObj;
     }
 
-    // Update is called once per frame
-    void Update()
+    public static TankCtrl GetObj(int objKind)
     {
-        
+        if (unitObjPool.tankPool[objKind].Count > 0)
+        {
+            var obj = unitObjPool.tankPool[objKind].Dequeue();
+            obj.transform.SetParent(null);
+            obj.gameObject.SetActive(true);
+            return obj;
+        }
+              
+        else
+        {
+            var newObj = unitObjPool.CreateNewObj(objKind);
+            newObj.transform.SetParent(null);
+            newObj.gameObject.SetActive(true);
+            return newObj;
+        }
     }
 
-    private void MakePool()
+    public static void ReturnObj(TankCtrl tank, int objKind)
     {
-
+        tank.gameObject.SetActive(false);
+        tank.transform.SetParent(unitObjPool.transform);
+        unitObjPool.tankPool[objKind].Enqueue(tank);
     }
 }
