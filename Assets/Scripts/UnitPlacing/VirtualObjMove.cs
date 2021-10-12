a6a2d94e165490f4b9fb6287b3c40e6776c8aa0a
LeeSeongKyu <80861684+LeeSeongKyu-920220@users.noreply.github.com>
2021-10-12 16:49:32 +0900

    유닛 배치 부분 추가

---
+++ Assets/Scripts/UnitPlacing/VirtualObjMove.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirtualObjMove : MonoBehaviour
+{
+    //======================================================================================== ↓ 변수 선언부
+    // 리얼 유닛 오브젝트
+    public GameObject realObj = null;
+
+    // 메테리얼 관련 변수   
+    public Material correctMtrl = null;     // 설치가 가능하면 보여줄 메테리얼
+    public Material denyMtrl = null;        // 설치가 안되면 보여줄 메테리얼
+    private new MeshRenderer renderer = new MeshRenderer();   // 메테리얼을 바꿔주기 위한 매쉬랜더러
+
+    private Vector3 targetObjPos = Vector3.zero;       // 생성할 오브젝트의 위치 변수    
+
+    Ray ray = new Ray();
+    RaycastHit hit = new RaycastHit();
+
+    bool isOccupied = false;
+    //======================================================================================== ↑ 변수 선언부
+
+
+    //======================================================================================== ↓ 유니티 함수 부분
+    //---------------------------------------------------------------------------- Start()
+    private void Start()
+    {
+        // 캐싱 부분
+        renderer = this.GetComponent<MeshRenderer>();       // 매쉬 랜더러 캐싱
+    }
+    //---------------------------------------------------------------------------- Start()
+
+    //---------------------------------------------------------------------------- Update()
+    private void Update()
+    {
+        Debug.Log("isOccupied = " + isOccupied);
+
+        // 오브젝트가 마우스를 따라가도록 함
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {            
+            targetObjPos = hit.point;
+            targetObjPos.y = 1.5f;
+            this.transform.position = targetObjPos;
+
+            // 배치 가능 구역으로 들어간다면 메테리얼을 초록색으로
+            if (hit.collider.gameObject.CompareTag("AbleZone") == true && isOccupied == false)
+            {
+                renderer.material = correctMtrl;
+            }            
+
+            else
+            {
+                renderer.material = denyMtrl;
+            }
+        }
+    }
+    //---------------------------------------------------------------------------- Update()
+
+    //---------------------------------------------------------------------------- OnCollisionEnter()
+    private void OnCollisionEnter(Collision col)
+    {
+        // 배치 가능 구역에 다른 물체가 있다면 설치를 못하게 해준다.
+        if (col.gameObject.CompareTag("AbleZone") == false)
+            isOccupied = true;
+    }
+    //---------------------------------------------------------------------------- OnCollisionEnter()
+
+    //---------------------------------------------------------------------------- OnCollisionExit()
+    private void OnCollisionExit(Collision col)
+    {
+        isOccupied = false;
+    }
+    //---------------------------------------------------------------------------- OnCollisionExit()
+
+    //======================================================================================== ↑ 유니티 함수 부분
+
+
+    //======================================================================================== ↓ 사용자 정의 함수 부분
+
+}
