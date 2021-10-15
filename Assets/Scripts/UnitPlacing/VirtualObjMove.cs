d4faa959b08415bcf9fde35e67c4ec861cbe065e
LeeSeongKyu <80861684+LeeSeongKyu-920220@users.noreply.github.com>
2021-10-15 17:58:59 +0900

    10/15 17:40 오브젝트 풀과 유닛 배치 연동 완료
    1. 이제 서버에서 받아온 정보로 유닛의 수의 제한이 가능합니다.
    
    2. 유닛은 제한 수 만큼 미리 생성되어 풀에 추가되며 필요한 경우에만 활성화 됩니다.
    
    3. 사용된 유닛은 원하는 조건에 맞추어 다시 풀로 되돌릴 수 있습니다.

--- Assets/Scripts/UnitPlacing/VirtualObjMove.cs
+++ Assets/Scripts/UnitPlacing/VirtualObjMove.cs
@@ -9,7 +9,7 @@ public class VirtualObjMove : MonoBehaviour
     public GameObject realObj = null;                  // 임시 오브젝트 (나중에 진짜 유닛 할당 필요)
 
     // 오브젝트의 관련 변수
-    [HideInInspector] public int objIndex = -1;                          // 어떤 탱크인지 알려주는 인덱스
+    private int objKind = -1;        // 어떤 탱크인지 알려주는 인덱스
     private Vector3 targetObjPos = Vector3.zero;       // 생성할 오브젝트의 위치 변수    
 
     // 메테리얼 관련 변수   
@@ -19,7 +19,7 @@ public class VirtualObjMove : MonoBehaviour
 
     // 위치 조정 및 상태 변화를 위한 변수
     Ray ray = new Ray();
-    RaycastHit hit = new RaycastHit();    
+    RaycastHit hit = new RaycastHit();
     bool isOccupied = false;                // 다른 물체가 있는지 확인을 위한 bool
 
     // UnitPlacing 의 상태 변화를 위한 변수
@@ -31,10 +31,12 @@ public class VirtualObjMove : MonoBehaviour
     //======================================================================================== ↓ 유니티 함수 부분
     //---------------------------------------------------------------------------- Start()
     private void Start()
-    {        
+    {
         // 캐싱 부분
         renderer = this.GetComponentsInChildren<MeshRenderer>();       // 매쉬 랜더러 캐싱
         unitPlacing = GameObject.FindObjectOfType<UnitPlacing>();   // unitPlacing 캐싱
+
+        objKind = (int)realObj.GetComponent<TankCtrl>().m_Type;
     }
     //---------------------------------------------------------------------------- Start()
 
@@ -44,29 +46,27 @@ public class VirtualObjMove : MonoBehaviour
         // 오브젝트가 마우스를 따라가도록 함
         ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out hit))
-        {            
+        {
             targetObjPos = hit.point;
-            targetObjPos.y = 1.55f;
+            targetObjPos.y = 1f;
             this.transform.position = targetObjPos;
 
             // 배치 가능 구역으로 들어간다면 메테리얼을 초록색으로
-            if (hit.collider.gameObject.CompareTag("AbleZone") == true 
-                || hit.collider.gameObject.CompareTag("Tank") == true
+            if (hit.collider.gameObject.CompareTag("AbleZone") == true
+                //|| hit.collider.gameObject.CompareTag("Tank") == true     // 겹치지 않게 배치하기 위해서는 이걸 빼야합니다.....!
                 && isOccupied == false)
             {
-                for(int ii = 0; ii < renderer.Length; ii++)
+                for (int ii = 0; ii < renderer.Length; ii++)
                 {
                     renderer[ii].material = correctMtrl;
                 }
-                
 
                 if (Input.GetMouseButtonDown(0))
                 {
-                    //Debug.Log("virtual : " + this.gameObject.transform.position);
                     MakeRealObj();
                     unitPlacing.placingState = UnitPlacingState.PRIMARY;        // 상태를 다시 원래대로
                 }
-            }            
+            }
 
             else
             {
@@ -87,8 +87,8 @@ public class VirtualObjMove : MonoBehaviour
         else
             isOccupied = false;
 
-        if (col.gameObject.CompareTag("Tank") == true)
-            isOccupied = false;
+        //if (col.gameObject.CompareTag("Tank") == true)
+        //    isOccupied = true;
     }
 
     private void OnTriggerExit(Collider col)
@@ -118,16 +118,16 @@ public class VirtualObjMove : MonoBehaviour
 
 
     //======================================================================================== ↓ 사용자 정의 함수 부분
+    // !!!!! 이걸 수정해야함.
     //---------------------------------------------------------------------------- MakeRealObj()
     //--------- 클릭시 진짜 오브젝트를 생성해주고 이 오브젝트는 파괴한다.
     private void MakeRealObj()
     {
-        //UnitObjPool.GetObj(objIndex, this.transform.position);             // 진짜 오브젝트 생산 (풀에서 꺼내옴)
-        
         Destroy(this.gameObject.GetComponent<Rigidbody>());     // 원래꺼가 밀어내는 거 방지를 위해 리지드 바디 삭제        
         Vector3 pos = this.transform.position;
-        pos.y = 1.0f;
-        Instantiate(realObj, pos, this.gameObject.transform.rotation);   // 임시 오브젝트 생산
+        pos.y = 1.0f;           // 맵과 위치 맞춰줌
+
+        UnitObjPool.Inst.GetObj(objKind, pos);             // 진짜 오브젝트 생산 (풀에서 꺼내옴)
         Destroy(this.gameObject, 0.08f);            // 약간 딜레이 주고 배치용 오브젝트 삭제        
     }
 
