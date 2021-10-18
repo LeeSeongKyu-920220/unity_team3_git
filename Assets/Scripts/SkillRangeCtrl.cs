f9f1e6ca4604cee2a196c8f1e1da5f0eee33bb76
choiseowon <92151567+choiseowon@users.noreply.github.com>
2021-10-18 17:32:18 +0900

    탱크 콜리더 수정 및 스킬 수정
    탱크가 폭격 스킬에 피해를 받도록 수정

--- Assets/Scripts/SkillRangeCtrl.cs
+++ Assets/Scripts/SkillRangeCtrl.cs
@@ -30,7 +30,16 @@ public class SkillRangeCtrl : MonoBehaviour
 
         for(int ii = 0; ii < target_List.Count; ii++)
         {
-            target_List[ii].GetComponent<EnemyCtrl>().Damage(50);
+            if (target_List[ii].name.Contains("Enemy") == true)
+                target_List[ii].GetComponent<EnemyCtrl>().Damage(50);
+            else
+            {
+                TankCtrl ctrl = target_List[ii].GetComponent<TankCtrl>();
+                if(ctrl != null)
+                    target_List[ii].GetComponent<TankCtrl>().TakeDamage(50);
+
+            }
+                
         }
 
         Destroy(this.gameObject);
@@ -40,14 +49,16 @@ public class SkillRangeCtrl : MonoBehaviour
 
     public void OnTriggerEnter(Collider coll)
     {
-        if (coll.name.Contains("Enemy") == true)
-            target_List.Add(coll.gameObject);
+        target_List.Add(coll.gameObject);
+        //if (coll.name.Contains("Enemy") == true)
+        //    target_List.Add(coll.gameObject);
     }
 
     public void OnTriggerExit(Collider coll)
     {
-        if (coll.name.Contains("Enemy") == true)
-            target_List.Remove(coll.gameObject);
+        target_List.Remove(coll.gameObject);
+        //if (coll.name.Contains("Enemy") == true)
+        //    target_List.Remove(coll.gameObject);
     }
 
     #endregion
